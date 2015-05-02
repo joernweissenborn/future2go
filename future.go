06cc4442b40a251483ce2be1b6a1a50ed563256b
joernweissenborn <joern.weissenborn@gmail.com>
2015-05-02 03:06:21 +0200

    initial code

--- future.go
+++ future.go
@@ -1,2 +1,103 @@
 package future2go
 
+import "sync"
+
+func New() (F *Future) {
+	F = new(Future)
+	F.m = new(sync.Mutex)
+	F.fcs = []futurecompleter{}
+	F.fces = []futurecompletererror{}
+	return
+}
+
+
+type CompletionFunc func(interface {}) interface {}
+type ErrFunc func(error) (interface {}, error)
+
+type Future struct {
+	m *sync.Mutex
+	fcs []futurecompleter
+	fces []futurecompletererror
+	c bool
+	r interface {}
+	e error
+}
+
+func (f *Future) Complete(Data interface {}){
+	f.m.Lock()
+	defer f.m.Unlock()
+	f.r = Data
+	for _, fc := range f.fcs {
+		deliverData(fc,Data)
+	}
+	f.c = true
+}
+
+func (f *Future) IsComplete() bool {
+	return f.c
+}
+
+func (f *Future) CompleteError(err error){
+	f.m.Lock()
+	defer f.m.Unlock()
+	f.e = err
+	for _, fce := range f.fces {
+		deliverErr(fce,f.e)
+	}
+	f.c = true
+}
+
+func deliverData(fc futurecompleter, d interface {}){
+	go func(){
+		fc.f.Complete(fc.cf(d))
+	}()
+}
+
+func deliverErr(fce futurecompletererror, e error){
+	go func(){
+		d, err := fce.ef(e)
+		if err == nil {
+			fce.f.Complete(d)
+		} else {
+			fce.f.CompleteError(err)
+		}
+	}()
+}
+
+func (f *Future) Then(cf CompletionFunc) (nf *Future) {
+	f.m.Lock()
+	defer f.m.Unlock()
+
+	nf = New()
+	fc := futurecompleter{cf,nf}
+	if f.c && f.e == nil {
+		deliverData(fc,f.r)
+	} else if !f.c  {
+		f.fcs = append(f.fcs,fc)
+	}
+	return
+}
+
+func (f *Future) Err(ef ErrFunc) (nf *Future) {
+	f.m.Lock()
+	defer f.m.Unlock()
+
+	nf = New()
+	fce := futurecompletererror{ef, nf}
+	if f.e != nil {
+		deliverErr(fce, f.e)
+	} else if !f.c{
+		f.fces = append(f.fces, fce)
+	}
+	return
+}
+
+type futurecompleter struct {
+	cf CompletionFunc
+	f *Future
+}
+
+type futurecompletererror struct {
+	ef ErrFunc
+	f *Future
+}
